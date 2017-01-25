b34b4c8e12f9e098f58c63d3f9781804b325c280
Kali Norby <kali@ifttt.com>
2017-01-25 15:47:16 -0800

    Changing timing of d4m-done removal

--- bin/d4m-nfs-start.sh
+++ bin/d4m-nfs-start.sh
@@ -2,8 +2,6 @@
 
 LIBDIR=$(dirname $0)
 
-rm -f ${LIBDIR}/d4m-done
-
 echo "[`date`][d4m-nfs] Waiting 30 seconds for the Docker VM to become ready"
 sleep 30
 echo "[`date`][d4m-nfs] Starting VM setup"
@@ -13,6 +11,7 @@ if ! $(screen -ls |grep d4m > /dev/null 2>&1); then
     echo "[`date`][d4m-nfs] Setup 'screen' to work properly with the D4M tty, while at it name it 'd4m'."
     screen -AmdS d4m ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
 
+    rm -f ${LIBDIR}/d4m-done
     echo "[`date`][d4m-nfs] Run Moby VM d4m-nfs setup script."
     screen -S d4m -p 0 -X stuff "${LIBDIR}/d4m-mount-nfs.sh
 "
